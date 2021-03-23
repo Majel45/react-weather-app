5680e536913aad68366ccb4ce47d57fe9a379894
Marilyn Greve majel45@icloud.com <majel45@icloud.com>
2021-03-23 11:19:22 +0100

    added css styling

--- src/Weather.js
+++ src/Weather.js
@@ -5,12 +5,12 @@ export default function Weather(){
   return (
     <div className="Weather">
       <form>
-        <div className="row">
+        <div className="row mt-3">
           <div className="col-9">
-            <input type="search" placeholder="Enter a city" className="form-control"/>
+            <input type="search" placeholder="Enter a city" className="form-control" autoFocus="on"/>
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary"/>
+            <input type="submit" value="Search" className="btn btn-primary w-100"/>
           </div>
         </div>      
       </form>
@@ -22,7 +22,8 @@ export default function Weather(){
       <div className="row">
         <div className="col-6">
           <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly cloudy"/>
-          7°C
+          <span className="temperature">7</span>
+          <span className="units">°C</span>
         </div>
         <div className="col-6">
           <ul>

2393a2909a1f5eff4548016739d4e56b159bb4ee
Marilyn Greve majel45@icloud.com <majel45@icloud.com>
2021-03-23 09:17:22 +0100

    added github repo url

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App"> Weather App</div>
+    <div className="App"> 
+    <h1>Weather App</h1> 
+    <footer>
+      This project was coded by Marilyn Greve and is{" "} 
+      <a href="https://github.com/Majel45/react-weather-app" target="_blank">
+        open-sourced on GitHub.
+      </a>
+    </footer>
+    </div>
   );
 }

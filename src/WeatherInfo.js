5654f174bb69762ed1c48aaf48e50ae261d3dbf7
Marilyn Greve majel45@icloud.com <majel45@icloud.com>
2021-03-29 15:50:55 +0200

    added the weather forecast component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,12 +13,16 @@ export default function WeatherInfo(props) {
           </li>
           <li className="text-capitalize">{props.data.description}</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
           <div className="col-6">
-            <div className="float-left">
-              <WeatherIcon code={props.data.icon}/>
+            <div className="clearfix">
+              <div className="float-left">
+                <WeatherIcon code={props.data.icon}/>
+              </div>
+              <div className="float-left">
+                <WeatherTemperature celsius={props.data.temperature}/>
+              </div>
             </div>
-            <WeatherTemperature celsius={props.data.temperature}/>
           </div>
           <div className="col-6">
             <ul>

3a4fecf3ad813937058909ae9bf2d08a3a041fb1
Marilyn Greve majel45@icloud.com <majel45@icloud.com>
2021-04-12 14:43:49 +0200

    changed the api forecast from hourly to daily

--- src/Weather.js
+++ src/Weather.js
@@ -10,6 +10,7 @@ export default function Weather(props){
   function handleResponse(response){
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       date: new Date(response.data.dt * 1000),
@@ -49,7 +50,7 @@ export default function Weather(props){
         </div>      
       </form>
       <WeatherInfo data={weatherData}/>
-      <WeatherForecast city={weatherData.city}/>
+      <WeatherForecast coordinates={weatherData.coordinates}/>
     </div>
     );
   } else {
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,29 +1,24 @@
 import React, { useState } from "react";
-import WeatherForecastPreview from "./WeatherForecastPreview";
 import WeatherIcon from "./WeatherIcon";
 import axios from "axios";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props){
-  const [loaded, setLoaded] = useState(false);
-  const [forecast, setForecast] = useState(null);
-
-  function handleForecastResponse(response){
-    setForecast(response.data);
-    setLoaded(true);
+  function handleResponse(response){
+    console.log(response.data);
   }
+  console.log(props);
 
-  if (loaded && props.city === forecast.city.name){
+  let apiKey = "38610e778dfb23c2f5bc0f2dc89e84b3";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
     return (
     <div className="WeatherForecast row">
       <div className="row mr-20">
         <div className="col">
-          {/* <WeatherForecastPreview data={forecast.list[0]}/>
-          <WeatherForecastPreview data={forecast.list[1]}/>
-          <WeatherForecastPreview data={forecast.list[2]}/>
-          <WeatherForecastPreview data={forecast.list[3]}/>
-          <WeatherForecastPreview data={forecast.list[4]}/>
-          <WeatherForecastPreview data={forecast.list[5]}/> */}
           <div className="WeatherForecast-day">Thu</div>
           <WeatherIcon code="01d" size={36}/>
           <div className="WeatherForecast-temperatures">
@@ -34,10 +29,4 @@ export default function WeatherForecast(props){
       </div>
     </div>
     );
-  } else {
-    let apiKey = "38610e778dfb23c2f5bc0f2dc89e84b3";
-    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
-    axios.get(url).then(handleForecastResponse);
-    return null;
-  }
-}
+  } 
